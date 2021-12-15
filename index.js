f95e18dd1c7834283e427ed3781b69d94d4f9842
Won-Kyung Chung <wkchung@kth.se>
2021-12-15 10:39:37 +0100

    Remove swaggerRedirectHandler and update package name and version to new major

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const createApiPaths = require('./lib/createApiPaths')
-const createSwaggerRedirectHandler = require('./lib/createSwaggerRedirectHandler')
 const { notFoundHandler, errorHandler } = require('./lib/errorHandlers')
 
 module.exports = {
   createApiPaths,
-  createSwaggerRedirectHandler,
   notFoundHandler,
   errorHandler,
 }
